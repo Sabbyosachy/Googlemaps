4ed0b84536bdc2b65f43c1d4ebed9b93606c2d99
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-10-21 21:24:15 +0600

    new

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const Map = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Map;
